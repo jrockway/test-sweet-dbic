db5a3c17b65880001b3b4a6ad20879e29e48a5f3
Jonathan Rockway <jon@jrock.us>
2009-11-03 16:34:29 -0600

    update example to work with latest version

--- t/Example.pm
+++ t/Example.pm
@@ -3,17 +3,14 @@ use MooseX::Declare;
 class t::Example {
     with 'Test::Sweet::DBIC' => { schema => 't::lib::Schema' };
     use Test::Sweet;
-    use Test::More;
-    use Test::Exception;
 
     has '+schema' => ( handles => ['resultset'] );
 
     test can_connect {
-        ok $_[0]->schema, 'have schema';
+        ok $self->schema, 'have schema';
     }
 
     test can_insert {
-        my $self = shift;
         my $row;
         lives_ok {
             $row = $self->resultset('Foo')->create({ number => 42 });
@@ -24,7 +21,6 @@ class t::Example {
     }
 
     test can_select {
-        my $self = shift;
         my $rs = $self->resultset('Foo')->search({ number => 42 });
         is $rs->count, 1, 'got 1 record';
         my $row = $rs->next;
